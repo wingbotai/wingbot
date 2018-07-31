d05dbd6c46acea2235e2c61c8c0c5bec2d7b7882
David Menger <david.menger@me.com>
2018-07-31 17:04:15 +0200

    Automatic continue

--- src/resolvers/inlineCode.js
+++ src/resolvers/inlineCode.js
@@ -5,8 +5,22 @@
 
 const { customFn } = require('./utils');
 
-function inlineCode (params) {
-    return customFn(params.code, params.description);
+function inlineCode (params, { isLastIndex }) {
+    const fn = customFn(params.code, params.description);
+
+    return async function (req, res, postBack, path, action) {
+        let ret = fn(req, res, postBack, path, action);
+
+        if (typeof ret === 'object' && ret !== null) {
+            ret = await ret;
+        }
+
+        if (typeof ret !== 'undefined') {
+            return ret;
+        }
+
+        return isLastIndex ? null : true;
+    };
 }
 
 module.exports = inlineCode;
