e0eb644c19388c77441829b743136875eff066b6
Ondrej Veres <ondra.veres@gmail.com>
2022-08-04 13:41:35 +0200

    downgraded smooth-doc, upgraged react and gatsby

--- documentation/gatsby-config.js
+++ documentation/gatsby-config.js
@@ -1,15 +1,16 @@
+'use strict';
 module.exports = {
-  pathPrefix: "/wingbot-documentation-deployment",
-  plugins: [
-    {
-      resolve: "smooth-doc",
-      options: {
-        name: "Wingbot Chatbot Framework Documentation",
-        description: "Wingbot Chatbot Framework Documentation",
-        siteUrl: "https://wingbotai.github.io/",
-        sections: ['BUILDING A CHATBOT WITH DESIGNER', 'UNDERSTANDING THE CORE', 'API'],
-        githubRepositoryURL: 'https://github.com/wingbotai/wingbot',
-      },
-    },
-  ],
+    pathPrefix: '/wingbot-documentation-deployment',
+    plugins: [
+        {
+            resolve: 'smooth-doc',
+            options: {
+                name: 'Wingbot Chatbot Framework Documentation',
+                description: 'Wingbot Chatbot Framework Documentation',
+                siteUrl: 'https://wingbotai.github.io/',
+                sections: ['BUILDING A CHATBOT WITH DESIGNER', 'UNDERSTANDING THE CORE', 'API'],
+                githubRepositoryURL: 'https://github.com/wingbotai/wingbot'
+            }
+        }
+    ]
 };
